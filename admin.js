5566776e156154ae904aa6d6dcce04f002999c6d
romulo100319 <romulodejesusarandia@gmail.com>
2026-02-11 19:24:12 +0800

    Fix: Anon Key updated

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://nrlsgrzqpduzzzphkhtn.supabase.co";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ybHNncnpxcGR1enp6cGhraHRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2MjYwNjQsImV4cCI6MjA4NjIwMjA2NH0.oQj3f76HaEHtweWu7vKTr1Atc1XYFq8gffv9eIO78Mc";
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // --- 1. SECURITY GUARD (Check if Admin) ---
